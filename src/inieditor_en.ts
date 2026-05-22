35a6821a0acc46f323347a87be26d490bcca7586
Jeremy Rimpo <jrim@rimpo.org>
2026-05-21 23:13:47 -0500

    Translation file updates

--- src/inieditor_en.ts
+++ src/inieditor_en.ts
@@ -4,50 +4,50 @@
 <context>
     <name>IniEditor</name>
     <message>
-        <location filename="inieditor.cpp" line="65"/>
+        <location filename="inieditor.cpp" line="59"/>
         <source>Very basic editor for INI files</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="inieditor.cpp" line="55"/>
-        <location filename="inieditor.cpp" line="84"/>
+        <location filename="inieditor.cpp" line="49"/>
+        <location filename="inieditor.cpp" line="83"/>
         <source>INI Editor</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="inieditor.cpp" line="89"/>
+        <location filename="inieditor.cpp" line="88"/>
         <source>Edit the INI files of the current profile</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="inieditor.cpp" line="100"/>
+        <location filename="inieditor.cpp" line="99"/>
         <source>plugin not initialized</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="inieditor.cpp" line="115"/>
+        <location filename="inieditor.cpp" line="114"/>
         <source> Editing profile-specific INIs. Profile-specific game INI files are Enabled.
-
+ 
  You can change the INI Editor settings (in the main Mod Organizer settings under the plugins tab) to use an external text editor,
  either notepad or your windows associated program to .INI files.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="inieditor.cpp" line="128"/>
+        <location filename="inieditor.cpp" line="120"/>
+        <source> Editing global INIs (in &apos;myGames&apos; folder). Profile-specific game INI files are Disabled.
+ 
+ You can change the INI Editor settings (in the main Mod Organizer settings under the plugins tab) to use an external text editor,
+ either notepad or your windows associated program to .INI files.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="inieditor.cpp" line="133"/>
         <source>File too big</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="inieditor.cpp" line="129"/>
+        <location filename="inieditor.cpp" line="134"/>
         <source>Sorry, the file %1 is too large to be handled by the INI Editor</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="inieditor.cpp" line="118"/>
-        <source> Editing global INIs (in &apos;myGames&apos; folder). Profile-specific game INI files are Disabled.
-
- You can change the INI Editor settings (in the main Mod Organizer settings under the plugins tab) to use an external text editor,
- either notepad or your windows associated program to .INI files.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
